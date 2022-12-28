3b817179fb82ce61b4e7d1d142750601f0e0e0fc
Aly Smith <alimay.smith@outlook.com>
2022-12-28 16:31:56 -0700

    implement meal detail page

--- app/js/main.js
+++ app/js/main.js
@@ -1,42 +1,54 @@
-console.log("Main");
 
-function addEventListeners(){
-  const recipes = document.getElementsByClassName('recipe');
-  console.log(recipes);
-  Array.from(recipes).forEach(r => {
-    r.addEventListener('click', function(event){
-    event.preventDefault();
-    let id = event.target.id;
-  });
-});
-}
-
+/*----------  Functions  ----------*/
 function searchDisplay(result){
   let resultList = document.getElementById('resultsList');
   resultList.innerHTML = '';
   result.forEach(r => {
     let li = document.createElement('li');
-    let link = document.createElement('p');
+    let link = document.createElement('a');
     let linkText = document.createTextNode(r.title);
       link.appendChild(linkText);
       link.setAttribute('class', 'recipe');
       link.setAttribute('id', r.id);
+      link.setAttribute('href', `html/mealDetail.html?id=${r.id}`);
       li.appendChild(link);
     resultList.appendChild(li);
   });
-  addEventListeners();
 }
 
+function detailDisplay(result){
+  let summary = document.getElementById('summary');
+  let title = document.getElementById('mealTitle');
+  let instructions = document.getElementById('instructions');
+  let ingredientList = document.getElementById('ingredientList');
 
-const mealSearch = document.getElementById('mealSearch');
-mealSearch.addEventListener('click', function(event) {
-  event.preventDefault();
-  let searchForm = document.getElementById('mealSearch');
-  let type = searchForm[0].value;
-  let value = searchForm[1].value;
-  apiConnect.search(type, value).then(result => searchDisplay(result));
-});
+  summary.innerHTML = result.summary;
+  title.innerText = result.title;
+  instructions.innerHTML = result.instructions;
 
+  result.extendedIngredients.forEach(i => {
+    let li = document.createElement('li');
+    li.innerText = i.original;
+    ingredientList.appendChild(li);
+  })
+}
 
+/*----------  Main ----------*/
+let page = window.location.href;
 
+if(page.includes('index.html')){
+  const mealSearch = document.getElementById('mealSearch');
+  mealSearch.addEventListener('click', function(event) {
+    event.preventDefault();
+    let searchForm = document.getElementById('mealSearch');
+    let type = searchForm[0].value;
+    let value = searchForm[1].value;
+    apiConnect.search(type, value).then(result => searchDisplay(result));
+  });
+}
+
+if (page.includes('mealDetail.html')) {
+  let rId = window.location.search.substring(1).split('=')[1];
+  apiConnect.getRecipeById(rId).then(result => detailDisplay(result));
+}
 
